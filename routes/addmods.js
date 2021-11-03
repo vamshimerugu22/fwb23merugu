79e68929a5c437f1fea95ab2f5c4ef0615488448
vamshimerugu22 <vamshi.ylgf66@gmail.com>
2021-11-03 12:09:15 -0500

    added bonus

--- routes/addmods.js
+++ routes/addmods.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   let query = req.query 
   console.log(`rows ${query.rows}`) 
   console.log(`cols ${query.cols}`) 
-  res.render('addmods', { title: 'Add Mods' });
+  res.render('addmods', { title: 'Add Mods', query:query });
 });
 
 module.exports = router;
